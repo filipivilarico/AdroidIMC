61516b22188efae011f69227a456dc427d98aa23
filipi <21193366@CFP127ALUNO.LOCAL>
2021-10-04 16:58:04 -0300

    oi

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.imcapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
+
+
+        val buttonEntrar = findViewById<Button>(R.id.buttonentrar)
+
+        buttonEntrar.setOnClickListener {
+            val abrirDashboard = Intent (this,DashboardActivity::class.java )
+            startActivity(abrirDashboard)
+        }
 
 
     }
